571f10b98c6c276ed0a02ca47e8a251115bb2650
je <qwer902181@gmail.com>
2023-08-11 15:54:19 +0800

    移除css變數改scss

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,4 @@
 
 <style lang="scss">
 @import "./assets/scss/all";
-
-:root{
-  --primary-text-color: #696969;
-  --secondary-text-color: black;
-  --other-text-color: rgb(171, 171, 171);
-  --dark-mode-text-color: #C5C5C5;
-  --dark-mode-secondary-text-color: rgb(255, 255, 255);
-  --dark-mode-compensation-color: rgba(209, 209, 209, 0.727);
-}
 </style>
