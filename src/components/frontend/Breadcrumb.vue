4c6e6865887c50c965796ab9fcf95f3bf5dac49b
je <qwer902181@gmail.com>
2023-08-11 16:04:31 +0800

    新增產品頁麵包屑

---
+++ src/components/frontend/Breadcrumb.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="breadcrumb">
+    <p> {{ title }} </p>
+    <p v-if="content">/ {{ content }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    content: String,
+  },
+};
+</script>
