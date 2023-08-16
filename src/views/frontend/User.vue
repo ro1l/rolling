7ff4ad9c0e4c9351bc88e8f57c6ad0ab5b794b82
je <qwer902181@gmail.com>
2023-08-16 16:11:41 +0800

    整理程式碼

--- src/views/frontend/User.vue
+++ src/views/frontend/User.vue
@@ -8,18 +8,11 @@
   <ToastList/>
 </template>
 
-<style lang="scss">
-.h-80{
-  margin-top: 80px;
-  background: #fff;
-}
-</style>
-
 <script>
-import Navbar from '@/components/frontend/Navbar.vue';
 import emitter from '@/methods/emitter';
 import pushMessageState from '@/methods/pushMessageState';
 import ToastList from '@/components/ToastList.vue';
+import Navbar from '@/components/frontend/Navbar.vue';
 import Footer from '@/components/frontend/Footer.vue';
 
 export default {
