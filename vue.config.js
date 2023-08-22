c8643be3aeee9245542fb748ee8edb0a0d51a726
je <qwer902181@gmail.com>
2023-08-22 13:24:54 +0800

    更改路徑

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
+/* eslint-disable no-param-reassign */
 module.exports = {
-  publicPath: '/dist/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/rolling/' : '/',
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = 'Rolling';
+      return args;
+    });
+  },
 };
