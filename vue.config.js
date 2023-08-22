a63219af35491b0781c6009804d8c4f437624243
je <qwer902181@gmail.com>
2023-08-22 13:05:13 +0800

    更改路徑

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
-const { defineConfig } = require('@vue/cli-service');
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/rolling/',
-});
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/rolling/'
+    : '/',
+};
