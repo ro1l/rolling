85c1804c01d885115245592ee7960473f6940bb2
ro1l <qwer902181@gmail.com>
2023-08-28 13:54:37 +0800

    手機版頁面換頁跳轉至頂部

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -5,7 +5,7 @@
       <!-- 上一頁 -->
       <li class="pre" v-if="pages.has_pre === true">
         <a href="#" aria-label="Previous"
-          @click.prevent="updatePage(pages.current_page - 1)"
+          @click="updatePage(pages.current_page - 1)"
           :class="{ 'disabled': pages.has_pre === false }">
           <span aria-hidden="true">上一頁</span>
         </a>
@@ -14,7 +14,7 @@
       <!-- 總頁數 -->
       <li class="page" v-for="page in pages.total_pages" :key="page"
         :class="{ 'active-page': page === pages.current_page }">
-        <a href="#" @click.prevent="updatePage(page)">
+        <a href="#" @click="updatePage(page)">
           {{ page }}
         </a>
       </li>
@@ -22,7 +22,7 @@
       <!-- 下一頁 -->
       <li class="next" v-if="pages.has_next === true">
         <a href="#" aria-label="Next"
-          @click.prevent="updatePage(pages.current_page + 1)"
+          @click="updatePage(pages.current_page + 1)"
           :class="{ 'disabled': pages.has_next === false }">
           <span aria-hidden="true">下一頁</span>
         </a>
